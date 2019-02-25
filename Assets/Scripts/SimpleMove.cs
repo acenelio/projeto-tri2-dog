9ea1d01b231635dbac5e0bde160de13c1019da0e
Nelio Alves <acenelio@gmail.com>
2019-02-24 21:53:56 -0300

    Incluindo limites para movimentacao na tela

--- Assets/Scripts/SimpleMove.cs
+++ Assets/Scripts/SimpleMove.cs
@@ -6,6 +6,14 @@ public class SimpleMove : MonoBehaviour
 {
     [SerializeField]
     private float SpeedFactor = 1f;
+    [SerializeField]
+    private float LimitTop = 4f;
+    [SerializeField]
+    private float LimitBottom = -4f;
+    [SerializeField]
+    private float LimitLeft = -8f;
+    [SerializeField]
+    private float LimitRight = 8f;
 
     void Start()
     {
@@ -20,5 +28,18 @@ public class SimpleMove : MonoBehaviour
         Vector3 velocity = new Vector3(horizontalInput, verticalInput, 0f) * SpeedFactor;
 
         transform.position = transform.position + velocity * Time.deltaTime;
+
+        if (transform.position.y < LimitBottom) {
+            transform.position = new Vector3(transform.position.x, LimitBottom, transform.position.z);
+        }
+        if (transform.position.y > LimitTop) {
+            transform.position = new Vector3(transform.position.x, LimitTop, transform.position.z);
+        }
+        if (transform.position.x < LimitLeft) {
+            transform.position = new Vector3(LimitLeft, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > LimitRight) {
+            transform.position = new Vector3(LimitRight, transform.position.y, transform.position.z);
+        }
     }
 }
