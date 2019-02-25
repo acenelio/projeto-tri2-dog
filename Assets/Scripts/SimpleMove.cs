dc5a0d6256902c5abb16d05875d9df2971f53ed2
Nelio Alves <acenelio@gmail.com>
2019-02-24 21:33:59 -0300

    Primeiro teste com input

--- Assets/Scripts/SimpleMove.cs
+++ Assets/Scripts/SimpleMove.cs
@@ -14,8 +14,10 @@ public class SimpleMove : MonoBehaviour
     
     void Update()
     {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
         float So = transform.position.x;
-        float S = So + SpeedFactor * Time.deltaTime;
+        float S = So + horizontalInput * SpeedFactor * Time.deltaTime;
         transform.position = new Vector3(S, transform.position.y, transform.position.z);
     }
 }
