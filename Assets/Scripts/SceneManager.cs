3ac95d8626479c96aad8fb98f537a26054afb3bc
Nelio Alves <acenelio@gmail.com>
2019-03-10 18:10:56 -0300

    Acrescentando assertions para proteger o codigo do jogo

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class SceneManager : MonoBehaviour
 {
@@ -8,6 +9,10 @@ public class SceneManager : MonoBehaviour
     private Vector3 TapeSpeed = new Vector3(-2f, 0f, 0f); 
     [SerializeField]
     private Transform Tape = null;
+
+    void Awake() {
+        Assert.IsNotNull(Tape);
+    }
 
     void Start()
     {
