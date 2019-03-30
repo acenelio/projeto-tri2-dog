2bc4d0caf05071ef2a46ba8e9aaf80fc849961f8
Nelio Alves <acenelio@gmail.com>
2019-03-30 16:54:45 -0300

    PlayerControl para detectar colisao com moedas e lancar particulas

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Coin")) {
+            SFXManager.instance.ShowCoinParticles(other.gameObject);
+            Destroy(other.gameObject);
+        }
+    }
+}
