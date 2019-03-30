3c7f7460debf466cf979582ef6bb88f19577d5b0
Nelio Alves <acenelio@gmail.com>
2019-03-30 16:45:33 -0300

    Criando script SFXManager

---
+++ Assets/Scripts/SFXManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour
+{
+    public static SFXManager instance = null;
+
+    public GameObject coinParticles;
+
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        }
+    }
+
+    public void ShowCoinParticles(GameObject obj) {
+        GameObject particles = Instantiate(coinParticles, obj.transform.position, Quaternion.identity);
+        GameObject tape = GameObject.Find("Tape");
+        particles.transform.SetParent(tape.transform);
+    } 
+}
