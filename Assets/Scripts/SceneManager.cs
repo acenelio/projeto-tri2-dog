1d164318b5f9da1d67d1a029097dbb1d5d113134
Nelio Alves <acenelio@gmail.com>
2019-03-30 20:17:55 -0300

    Contando moedas

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -5,6 +5,8 @@ using UnityEngine.Assertions;
 
 public class SceneManager : MonoBehaviour
 {
+    public static SceneManager instance = null;
+
     [SerializeField]
     private Vector3 TapeSpeed = new Vector3(-2f, 0f, 0f); 
     [SerializeField]
@@ -12,18 +14,26 @@ public class SceneManager : MonoBehaviour
 
     public UIComponents uiComponents;
 
+    SceneData sceneData = new SceneData();
+
     void Awake() {
         Assert.IsNotNull(Tape);
+        if (instance == null) {
+            instance = this;
+        }
     }
-
-    void Start()
-    {
-        
-    }
-
     
     void Update()
     {
         Tape.position = Tape.position + TapeSpeed * Time.deltaTime;
+        DisplayHudData();
+    }
+
+    public void IncrementCoinCount() {
+        sceneData.coinCount++;
+    }
+
+    void DisplayHudData() {
+        uiComponents.hud.txtCoinCount.text = "x " + sceneData.coinCount;
     }
 }
