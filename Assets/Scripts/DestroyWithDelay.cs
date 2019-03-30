2e96b8d252b923db237391c4f1f16b41965d1415
Nelio Alves <acenelio@gmail.com>
2019-03-30 16:59:13 -0300

    Script DestroyWithDelay para destruir as particulas apos um tempo

---
+++ Assets/Scripts/DestroyWithDelay.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWithDelay : MonoBehaviour
+{
+    public float delay = 2f;
+
+    void Start()
+    {
+        Destroy(gameObject, delay);
+    }
+}
