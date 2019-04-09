26e6b5d7f5a54b0485b60a43045e32c0635b2fff
Nelio Alves <acenelio@gmail.com>
2019-04-09 09:52:43 -0300

    Matando o player por encostar no canto esquerdo Forbidden

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -12,11 +12,19 @@ public class PlayerControl : MonoBehaviour
             LevelManager.instance.IncrementCoinCount();
         }
         else if (other.gameObject.layer == LayerMask.NameToLayer("Enemies")) {
-            Camera.main.GetComponentInChildren<AudioSource>().mute = true;
-            LevelManager.instance.SetTapeSpeed(0);
-            AudioManager.instance.PlaySoundFail(gameObject);
-            SFXManager.instance.ShowDieParticles(gameObject);
-            Destroy(gameObject);
+            KillPlayer();
+        }
+        else if (other.gameObject.layer == LayerMask.NameToLayer("Forbidden")) {
+            KillPlayer();
         }
     }
+
+    void KillPlayer() {
+        Camera.main.GetComponentInChildren<AudioSource>().mute = true;
+        LevelManager.instance.SetTapeSpeed(0);
+        AudioManager.instance.PlaySoundFail(gameObject);
+        SFXManager.instance.ShowDieParticles(gameObject);
+        Destroy(gameObject);        
+    }
+
 }
